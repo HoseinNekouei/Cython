e7f13115adb2517d4b513a113826a8ea4b337606
Hosein Nekouei <nekouei.hosein@gmail.com>
2023-01-21 23:30:14 +0330

    Revise the file to create fixed data

--- data.py
+++ data.py
@@ -3,5 +3,15 @@ import numpy as np
 def getarray():
     return array
 
-# array = np.arange(1,5,0.0004,dtype='f')
-array = np.random.random_sample((2000,))
+# array = np.random.randint(100001,999999,2000)
+
+# for item in array:
+#     with open('data.txt','a') as f:
+#         f.write(str(item))
+#         f.write(',')
+        
+
+with open('data.txt','r') as f:
+    array = f.read().rstrip(',').split(',')
+    array = [int(item) for item in array]
+    array = np.array(array)
